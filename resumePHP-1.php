2237f83d8ee709a8385639a1c5bec90a045cabfd
kyyzen <146609954+kyyzen@users.noreply.github.com>
2023-10-22 12:53:19 +0700

    Add files via upload

---
+++ resumePHP-1.php
@@ -0,0 +1,59 @@
+<?php
+    Echo "Hello World";
+    ECHO "Hello World";
+    EcHo "Hello World";
+
+$andi="Andi";
+echo $Andi;
+
+$i;
+$nama;
+$umur;
+$_lokasi_memori;
+$ANGKA_MAKSIMUM;
+
+$a='Saya Sedang belajar PHP';
+$b=5;
+print $a;
+echo $b;
+
+$a=5;
+$b="Sedang belajar PHP $a";
+echo $b;
+//hasil: Saya sedang belajar PHP 5
+
+$umur=21;
+$harga=15000;
+$rugi=-500000;
+
+echo $umur; //21
+echo"<br />";
+echo $harga; //15000
+echo "<br />";
+echo $rugi; //-500000
+
+$a=14;
+$b=16;
+$c=$a*$b;
+echo $c; //30
+$d=$a*$b;
+echo $d; //224
+
+$angkas_float1= 0.78;
+$angkas_float2= 14.99;
+$angkas_scientific1=0.314E1;
+$angkas_scientific2=0.3365E-3;
+
+echo $angkas_float1; //0.78
+echo "<br />";
+echo $angkas_scientific2; //14.99
+echo "<br />";
+echo $angkas_scientific1; //3.14
+echo $angkas_scientific2; //0.0003365
+
+$a=10.66;
+$b=12.4;
+$c=$a + $b;
+echo $c; //23.00
+?>
+
