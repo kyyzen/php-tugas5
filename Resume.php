f4968a143388577bead24991df112dcdcaf78c13
kyyzen <146609954+kyyzen@users.noreply.github.com>
2023-10-29 04:00:18 +0700

    Add files via upload

---
+++ Resume.php
@@ -0,0 +1,94 @@
+<?php
+    Echo "Hello World";
+    ECHO "Hello World";
+    EcHo "Hello World";
+
+$andi="Andi";
+echo $Andi;
+
+$i;
+$nama;
+$umur;
+$_lokasi_memori;
+$ANGKA_MAKSIMUM;
+
+$a='Saya Sedang belajar PHP';
+$b=5;
+print $a;
+echo $b;
+
+$a=5;
+$b="Sedang belajar PHP $a";
+echo $b;
+//hasil: Saya sedang belajar PHP 5
+
+$umur=21;
+$harga=15000;
+$rugi=-500000;
+
+echo $umur; //21
+echo"<br />";
+echo $harga; //15000
+echo "<br />";
+echo $rugi; //-500000
+
+$a=14;
+$b=16;
+$c=$a*$b;
+echo $c; //30
+$d=$a*$b;
+echo $d; //224
+
+$angkas_float1= 0.78;
+$angkas_float2= 14.99;
+$angkas_scientific1=0.314E1;
+$angkas_scientific2=0.3365E-3;
+
+echo $angkas_float1; //0.78
+echo "<br />";
+echo $angkas_scientific2; //14.99
+echo "<br />";
+echo $angkas_scientific1; //3.14
+echo $angkas_scientific2; //0.0003365
+
+$a=10.66;
+$b=12.4;
+$c=$a + $b;
+echo $c; //23.00
+
+$d=$a / $b;
+echo $d; //0.85967741935484
+
+$string1='ini adalah string sederhana';
+$string2="ini adalaha string yang bisa memiliki beberapa baris";
+$string3='Dia berkata: "I\'ll be back"';
+
+echo $string1; echo "<br>";
+echo $string2; echo "<br>";
+echo $string3; echo "<br>";
+
+$benar=true;
+$salah=false;
+
+echo "benar = $benar, salah = $salah";
+//hasil output: benar= 1, salah =
+
+$hasil1= -3;
+$hasil2=3+5;
+$hasil3=8-4.5;
+$hasil4=2*5;
+$hasil5=3+8/5-3;
+$hasil6=10 % 4;
+
+echo "$hasil1:"; var_dump($hasil1);
+echo "<br \>";
+echo "$hasil2:"; var_dump($hasil2);
+echo "<br \>";
+echo "$hasil3:"; var_dump($hasil3);
+echo "<br \>";
+echo "$hasil4:"; var_dump($hasil4);
+echo "<br \>";
+echo "$hasil5:"; var_dump($hasil5);
+echo "<br \>";
+echo "$hasil6:"; var_dump($hasil6);
+?>
